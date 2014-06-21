b68d1f826c559b28e62f3af27b19eeee41a21245
SahuRahul <zoom.rahul@gmail.com>
2014-06-21 17:13:50 -0500

    Executes matrix inversion & store results in cache
    Executes matrix inversion & store results in cache, so that next time
    you need the inversion, you don`t have to calculate

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,38 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## These function take in a matrix and calculate its inverse. The inverse is
+## stored to memory, so that next time you need the inverse, you don`t need to
+## calculate it, you can simply retrieve
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+    message("Storing matrix")
+  }
+  
+  get <- function()
+    {
+    message("Return matrix")
+    x     
+  }
+  
+  setsolve <- function(solve){
+    message("Setting matrix inversion to repository")
+    m <<- solve
+  } 
+  
+  getsolve <- function()
+    {
+    message("Checking for existing matrix inversion")
+    m
+  }
+  
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
@@ -12,4 +40,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m<-x$getsolve()
+  if(!is.null(m)) {
+    message("Cache data found!!")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
